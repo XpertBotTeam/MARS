7523f5b46562a97cf518af08f635b45259618ea9
jadberjawi <jadberjawi9@gmail.com>
2022-08-09 17:45:44 +0300

    Login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,9 +8,17 @@ class LoginController extends Controller
 {
     //
     public function index(){
-
+    redirect('/');
     }
     public function store(){
-
+        $validated = \request()->validate([
+            'email' => ['required',Rule::exists('users','email')],
+            'password' => 'required'
+        ]);
+        if(auth()->attempt($validated)){
+            session()->regenerate();
+            redirect('/')->with('success',' user logged in');;
+        }
     }
+
 }
