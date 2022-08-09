379d92fe601bd90392f02278ac5372ad750c0162
jadberjawi <jadberjawi9@gmail.com>
2022-08-09 17:49:18 +0300

    Register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
     //
+    public function index(){
+        redirect('/');
+    }
+    public function store(){
+        $validated = \request()->validate([
+            'username' => [Rule::unique('users','username'),'required','max:255'],
+            'email'=> [Rule::unique('users','email'),'required','email','max:255'],
+            'password'=> ['required','max:255','min:7']
+        ]);
+
+        $user=User::create($validated);
+        auth()->login($user);
+
+        return redirect('/')->with('success','your user have been created');
+    }
 }
