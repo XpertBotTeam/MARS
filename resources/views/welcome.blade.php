19f5fe23d4a3cfb798f3cde9378d7af55284b74c
jadberjawi <jadberjawi9@gmail.com>
2022-08-09 13:35:17 +0300

    React designing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <!-- Styles -->
-    <link href="./resources/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-
 <body>
 
 <!-- React root DOM -->
-<div id="example">
+<div id="user">
+    Here
 </div>
-
 <!-- React JS -->
-<script src="./resources/js/app.jsx" defer></script>
-
+<script src="./resources/js/app.js" defer></script>
 </body>
 </html>
