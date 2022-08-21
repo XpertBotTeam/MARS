83e800db73b9c96e7ea3ba2ff33d8a5265386ab7
jadberjawi <jadberjawi9@gmail.com>
2022-08-21 15:39:50 +0300

    services request done

---
+++ app/Http/Controllers/ServiceRequestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServiceRequestController extends Controller
+{
+    //
+    public function store()
+    {
+
+    }
+}
